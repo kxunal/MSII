c0631acb7065cf4d00add185dcd2ee2baff719ec
ᴋᴜɴᴀʟ <155554911+VARC9210@users.noreply.github.com>
2024-06-30 13:32:08 +0530

    Update start.py

--- VIPMUSIC/utils/inline/start.py
+++ VIPMUSIC/utils/inline/start.py
@@ -30,7 +30,7 @@ def private_panel(_):
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),             
         ],
         [
-            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),   
+            InlineKeyboardButton(text=_["S_B_11"], user_id=config.OWNER_ID),   
             InlineKeyboardButton(
                 text=_["S_B_10"],
                 url=f"https://t.me/StormChatter_bot?startgroup=true&admin=delete_messages+invite_users",
