c2fca905f76566b3c6526af560690ab29d71c976
ᴋᴜɴᴀʟ <155554911+VARC9210@users.noreply.github.com>
2024-06-30 13:50:26 +0530

    Update play.py

--- VIPMUSIC/utils/inline/play.py
+++ VIPMUSIC/utils/inline/play.py
@@ -34,7 +34,8 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
     duration_sec = time_to_seconds(dur)
     percentage = (played_sec / duration_sec) * 100
     umm = math.floor(percentage)
-    if 0 < umm <= 40:
+    
+    if 0 < umm <= 10:
         bar = "◉——————————"
     elif 10 < umm < 20:
         bar = "—◉—————————"
@@ -46,27 +47,27 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
         bar = "————◉——————"
     elif 50 <= umm < 60:
         bar = "——————◉————"
-    elif 50 <= umm < 70:
+    elif 60 <= umm < 70:
         bar = "———————◉———"
     else:
         bar = "——————————◉"
-
-buttons = [
-    [
-        InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
-        InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
-        InlineKeyboardButton(text="↺", callback_data=f"ADMIN Replay|{chat_id}"),
-        InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
-        InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
-    ],
-    [
-        InlineKeyboardButton(
-            text=f"{played} {bar} {dur}",
-            callback_data="GetTimer",
-        )
-    ],
-    [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
-]
+    
+    buttons = [
+        [
+            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="↺", callback_data=f"ADMIN Replay|{chat_id}"),
+            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
+        ],
+        [
+            InlineKeyboardButton(
+                text=f"{played} {bar} {dur}",
+                callback_data="GetTimer",
+            )
+        ],
+        [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
+    ]
     return buttons
 
 
