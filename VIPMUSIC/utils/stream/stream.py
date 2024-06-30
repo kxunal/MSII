b2f2bb1906ea9ed8a097c565b35342660c5f7a96
ᴋᴜɴᴀʟ <155554911+VARC9210@users.noreply.github.com>
2024-06-30 19:45:37 +0530

    Update stream.py

--- VIPMUSIC/utils/stream/stream.py
+++ VIPMUSIC/utils/stream/stream.py
@@ -53,14 +53,7 @@ from VIPMUSIC.core.call import VIP
 from VIPMUSIC.misc import db
 from VIPMUSIC.utils.database import add_active_video_chat, is_active_chat
 from VIPMUSIC.utils.exceptions import AssistantErr
-from VIPMUSIC.utils.inline import (
-    aq_markup,
-    queuemarkup,
-    close_markup,
-    stream_markup,
-    stream_markup2,
-    panel_markup_4,
-)
+from VIPMUSIC.utils.inline import aq_markup, close_markup, stream_markup)
 from VIPMUSIC.utils.pastebin import VIPBin
 from VIPMUSIC.utils.stream.queue import put_queue, put_queue_index
 from youtubesearchpython.__future__ import VideosSearch
