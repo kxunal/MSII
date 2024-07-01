afc2abc430b4ed290e14d7e8eb285d01e7f8563e
ᴋᴜɴᴀʟ <155554911+VARC9210@users.noreply.github.com>
2024-07-01 12:43:10 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,17 +1,2 @@
 #!/bin/bash
-
-set -e
-
-export FLASK_APP=kex:create_app
-
-gunicorn -w 4 -b 0.0.0.0:${PORT:-8080} kex:create_app
-
-sleep 5
-
-FLASK_APP_URL="http://0.0.0.0:${PORT:-8080}"
-until curl -s $FLASK_APP_URL > /dev/null; do
-    echo "ᴡᴀɪᴛɪɴɢ ꜰᴏʀ ꜰʟᴀꜱᴋ ᴀᴘᴘ ᴛᴏ ꜱᴛᴀʀᴛ..."
-    sleep 2
-done
-
-python3 -m VIPMUSIC
+python3 kex.py & python3 -m VIPMUSIC
