f98bcee5a87545bef2a4baf89da6c7e343e6ba03
ᴋᴜɴᴀʟ <168260488+kxunal@users.noreply.github.com>
2024-07-11 18:44:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -33,7 +33,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/VARC9210/MSI",
+    "https://github.com/kxunal/MSII",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
